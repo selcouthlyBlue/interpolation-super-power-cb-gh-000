c5eebec9a1f320fe936f329f3ae7d6f324853dbd
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 02:11:47 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,6 @@
 def display_rainbow(colors)
   rainbow = ""
   colors.each{ |color| rainbow += "#{color[0].upcase}: #{color}, "  }
-  rainbow = rainbow.slice(-1)
-  rainbow = rainbow.slice(-1)
+  rainbow = rainbow.chomp(', ')
   puts rainbow
 end
