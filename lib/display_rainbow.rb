f5ffa8524d49afa6ee9a932ef5b14e4c6de3472d
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 02:30:41 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,11 @@
 # Write your #display_rainbow method here
 
-def display_rainbow(colors)
-  colors.each{ |color| puts "#{color.initial.upcase}: color"  }
+def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  print "#{colors[0][0].upcase}: #{colors[0]}, "
+  print "#{colors[1][0].upcase}: #{colors[1]}, "
+  print "#{colors[2][0].upcase}: #{colors[2]}, "
+  print "#{colors[3][0].upcase}: #{colors[3]}, "
+  print "#{colors[4][0].upcase}: #{colors[4]}, "
+  print "#{colors[5][0].upcase}: #{colors[5]}, "
+  puts "#{colors[6][0].upcase}: #{colors[6]}"
 end
