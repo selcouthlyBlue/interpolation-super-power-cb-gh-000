aae7952f53c417bd32fc473be726194847585f8b
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 02:04:16 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  colors.each{ |color| print "#{color[0].upcase}: #{color} "  }
+  colors.each{ |color| print "#{color[0].upcase}: #{color},"  }
 end
