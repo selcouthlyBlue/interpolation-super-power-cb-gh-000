6ff3ab5abe8a6c43284a195e407afc3d019dbda3
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 02:22:26 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  rainbow = ""
+  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   colors.each{ |color| rainbow += "#{color[0].upcase}: #{color}, "  }
   rainbow = rainbow.chomp(', ')
   puts rainbow
